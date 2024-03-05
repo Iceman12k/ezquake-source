75e35bb4f400860038074d776b992af641742125
Ad Infinitum <67801374+inf1niti@users.noreply.github.com>
2024-03-05 11:58:47 -0500

    LIGHTMAPS: Add support for DECOUPLEDLM (CPU).
    migrate commit #9edc63f

--- gl_model.h
+++ gl_model.h
@@ -205,6 +205,10 @@ typedef struct msurface_s {
 	struct	msurface_s  *drawflatchain;
 
 	mtexinfo_t          *texinfo;
+
+	// decoupled lm
+	float               lmvecs[2][4];
+	float               lmvlen[2];
 	
 	// lighting info
 	int                 dlightframe;
